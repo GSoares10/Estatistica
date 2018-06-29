97244941e6e758ac212a26a800ba508927939c4a
Gabriel Soares <37085637+GSoares10@users.noreply.github.com>
2018-06-29 17:04:20 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("js/main.js");
+
+const tabela = document.getElementById('table0');
+const button = document.getElementById('gerar');
+const cabecalho = document.getElementById('cabecalho');
+const titulo = document.getElementById('titulo');
+
+// Script here \/;
+
+button.addEventListener('click', function() {
+    const event = tabela.children.length;
+    for(let i=0; i < event; i++) {
+        document.querySelector('#table0');this.children[0];
+    }
+    //Chamar função das frequencias e concatenar os dados na tabela.
+});
