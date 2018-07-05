04e328fe5d4487bd4143978a23e87dc964f98c62
Gabriel Soares <37085637+GSoares10@users.noreply.github.com>
2018-07-04 23:17:19 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,94 +1,118 @@
 console.log('js/main.js');
 
-const tabela = document.getElementById('table0');
-const button = document.getElementById('gerar');
-const caption = document.querySelector('caption');
-const thead = document.querySelector('thead');
-const tbody = document.querySelector('tbody');
-const span = document.querySelector('span');
-const cabecalho = document.getElementById('cabecalho');
-const titulo = document.getElementById('titulo');
-const dados = document.getElementById('dados');
-const fi = document.getElementById('Fi');
-const fai = document.getElementById('FAi');
-const fr = document.getElementById('Fr');
-const far = document.getElementById('FAr');
-// Script here \/;
+document.querySelector("#enviar").addEventListener("click", gerarTabela);
+document.querySelector("#excluir").addEventListener("click", excluirTabela);
 
-button.addEventListener('click', function() {
-    let tabFi = [];
-    let tabFAi = [];
-    let tabFr = [];
-    let tabFAr = [];
-    const event = tabela.children.length;
+var linha;
 
-    /* for (let i=0; i < event; i++) {
-        document.querySelector('#table0'); this.children[0];
-    } estava dando problema com o this.children[0],
-     entao comentei para poder testar o resto do codigo*/
+function gerarTabela() {
+    if (document.querySelector("#nomeTabela").value != "" && document.querySelector("#nomeDado").value != "" && document.querySelector("#dados").value != "" && document.querySelector("#fonte").value != "") {
+        //pegar dados dos inputs
+        document.querySelector("#nometable").innerHTML = document.querySelector("#nomeTabela").value
 
-    function calcfai() {
-        const vet = [];
-        let aux = 0;
-        for (let i = 0; i < dados.value.split(' ').length; i++) {
-            aux += parseInt(dados.value.split(' ')[i]);
-            vet.push(aux);
-            // console.log(tabfai);
+        document.querySelector("#nome").innerHTML = document.querySelector("#nomeDado").value
+
+        document.querySelector("#fontetable").innerHTML = "Fonte: " + document.querySelector("#fonte").value;
+
+        dados = document.querySelector("#dados").value.split(",");
+
+        //organizar dados
+        dados.sort(function (a, b) {
+            return a - b
+        });
+
+        if (dados.indexOf("QWERTYUIOPASDFGHJKLÇZXCVBNMqwertyuiopasdfghjklçzxcvbnm") != -1) {
+            for (c = 0; c < dados.length; c++) {
+                dados[c] = parseFloat(dados[c]);
+            }
         }
-        return vet;
-    }
 
-    function calcfr() {
-        const vet = [];
-        const aux2 = calcfai();
-        for (let i = 0; i < dados.value.split(' ').length; i++) {
-            const aux3 = ((parseInt(dados.value.split(' ')[i]) * 100)
-             / aux2[aux2.length - 1]);
-            vet.push(aux3);
+        linhas = [];
+        for (c = 0; c < dados.length; c++) {
+            if (linhas.indexOf(dados[c]) == -1) linhas.push(dados[c]);
         }
-        return vet;
-    }
-    if (Fi.checked === true) {
-        tabFi = (dados.value.split(' '));
-    } else {
-        tabFi = false;
-    }
-
-    if (FAi.checked === true) {
-        tabFAi = calcfai();
-        console.log(tabFAi);
-    } else {
-        tabFAi = false;
-    }
-
-    if (Fr.checked === true) {
-        tabFr = calcfr();
-    } else {
-        tabFr = false;
-    }
-    if (FAr.checked === true) {
-        const vetaux = calcfr();
-        let aux = 0;
-        for (let i = 0; i < vetaux.length; i++) {
-            aux += vetaux[i];
-            tabFAr.push(aux);
+        var frequencias = [];
+        for (c = 0; c < linhas.length; c++) {
+            var freq = 0;
+            for (c1 = 0; c1 < dados.length; c1++) {
+                if (dados[c1] == linhas[c]) freq++;
+            }
+            frequencias[c] = freq;
         }
+
+        //criar tabela
+        var tabela = document.querySelector('#tabela');
+
+        for (c = 0; c < linhas.length; c++) {
+            var tr = document.createElement('tr');
+            var linha = document.createElement('td');
+            var fi = document.createElement('td');
+            var fai = document.createElement('td');
+            var fr = document.createElement('td');
+            var fri = document.createElement('td');
+
+            linha.textContent = linhas[c];
+            fi.textContent = frequencias[c];
+            var faiValor = 0;
+            for (c1 = 0; c1 <= c; c1++) {
+                faiValor += frequencias[c1];
+            }
+            fai.textContent = faiValor;
+
+            fr.textContent = (frequencias[c] / dados.length) * 100 + "%";
+
+            var friValor = 0;
+            for (c1 = 0; c1 <= c; c1++) {
+                friValor += (frequencias[c1] / dados.length) * 100;
+            }
+            fri.textContent = friValor + "%";
+
+            tr.appendChild(linha);
+            tr.appendChild(fi);
+            tr.appendChild(fai);
+            tr.appendChild(fr);
+            tr.appendChild(fri);
+
+            tabela.appendChild(tr);
+        }
+        var tr = document.createElement('tr');
+        var linha = document.createElement('td');
+        var fi = document.createElement('td');
+        var fai = document.createElement('td');
+        var fr = document.createElement('td');
+        var fri = document.createElement('td');
+
+        linha.textContent = "Total";
+        fi.textContent = dados.length;
+        fai.textContent = "-";
+        fr.textContent = "100%";
+        fri.textContent = "-";
+
+        tr.appendChild(linha);
+        tr.appendChild(fi);
+        tr.appendChild(fai);
+        tr.appendChild(fr);
+        tr.appendChild(fri);
+
+        tabela.appendChild(tr);
+        document.querySelector("#enviar").disabled = true;
+        document.querySelector("#excluir").disabled = false;
+        document.querySelector('#TABELA').style.display = "inline";
     } else {
-        tabFAr = false;
+        //alert("Preencha todos os campos por favor!");
     }
-    
-    caption.textContent = titulo.value;
-    span.textContent = 'Fonte: '+fonte.value;
-    thead.innerHTML = `<td>Fi</td><td>FAi</td><td>Fr</td><td>Fr</td>`
+}
 
-    for (let i = 0; i <= dados.length; i++) {
-        tbody.innerHTML += `<tr><td>${cabecalho.value[i]}</td><td>${tabFi[i]}</td>
-        <td>${tabFAi[i]}</td><td>${tabFr[i]}</td><td>${tabFAr[i]}</td></tr>`;
-    }   
-    
-    // table0.innerHTML += `<thead><tr><th>${cabecalho.value}</th><th hidden>Fi</th><th hidden>FAi</th><th hidden>Fr</th><th hidden>FAr</th></tr></thead>`;
-    // table0.innerHTML += `<thead><tr><th>${tabFi}</th><th>${tabFAi}</th><th>${tabFr}</th><th>${tabFAr}</th></tr></thead>`;
-    //table0.innerHTML = `<span>Fonte:${document.getElementById('fonte').value}</span>`
-    // Chamar função das frequencias e concatenar os dados na tabela.
+function excluirTabela() {
+    var elementos = document.querySelector('#tabela').childNodes;
+    var tamanho = elementos.length;
+    for (x = 2; x < tamanho; x++) {
+        document.querySelector('#tabela').removeChild(document.querySelector("#tabela").lastChild);
+    }
+    document.querySelector("#enviar").disabled = false;
+    document.querySelector("#excluir").disabled = true;
+    document.querySelector('#TABELA').style.display = "none";
+    document.querySelector('#nometable').innerHTML = "";
+    document.querySelector('#fontetable').innerHTML = "";
 
-});
+}
